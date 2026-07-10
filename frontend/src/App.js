07407ee366a91577b1f189c0a7cabbad8d7e4f57
Gaurav Soni <gaurav.soni@aicentre.org>
2026-07-10 21:48:53 +0530

    change in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Route exact path='/dashboard@admin'>
             {user ? (user.isAdmin === true ? <AdminDashboard /> : <Redirect to='/' />) : <Redirect to='/' />}
           </Route>
-          <Route exact path='/books'>
+          <Route exact path='/mybooks'>
             <Allbooks />
           </Route>
         </Switch>
